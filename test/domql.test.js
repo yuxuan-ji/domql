f17a50a318a9fefd54288b39147357c8e7772c02
Y.J <yuxuan98@hotmail.com>
2019-02-18 16:58:46 -0500

    Setup basic mocha browser test

--- test/domql.test.js
+++ test/domql.test.js
@@ -1,18 +1,7 @@
 /* global describe, it, before */
 
-import chai from 'chai';
-import {Domql} from '../lib/domql.js';
-
-chai.expect();
-
-const expect = chai.expect;
-
-let lib;
-
 describe('Given an instance of my domql library', () => {
-  before(() => {
-    lib = new Domql();
-  });
+  var lib = new domql.Domql();
   describe('when I need the name', () => {
     it('should return the name', () => {
       expect(lib.name).to.be.equal('Domql');
