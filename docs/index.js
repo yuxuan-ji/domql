52fa804a1b1a70165e8974a8680716d5aa226428
Y.J <yuxuan98@hotmail.com>
2019-03-25 11:37:13 -0400

    add gh-pages

--- docs/index.js
+++ docs/index.js
@@ -1,2 +1,70 @@
-var converter = new showdown.Converter({'ghCodeBlocks': true});
-document.getElementById("html-mdown").innerText = html_beautify(document.querySelector('*').innerHTML, {indent_size: 2});
+// Sidebar button
+document.addEventListener('DOMContentLoaded', function(event) {
+  // Get the Sidebar
+  var sidebar = document.getElementById("sidebar");
+
+  // Get the DIV with overlay effect
+  var overlayBg = document.getElementById("overlay");
+
+  // Toggle between showing and hiding the sidebar, and add overlay effect
+  function w3_open() {
+    if (sidebar.style.display === 'block') {
+      sidebar.style.display = 'none';
+      overlayBg.style.display = "none";
+    } else {
+      sidebar.style.display = 'block';
+      overlayBg.style.display = "block";
+    }
+  }
+
+  // Close the sidebar with the close button
+  function w3_close() {
+    sidebar.style.display = "none";
+    overlayBg.style.display = "none";
+  }
+});
+
+// Set current page markup to innerHTML
+converter = new showdown.Converter({'ghCodeBlocks': true});
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  var content = html_beautify(document.querySelector('*').innerHTML, {indent_size: 2});
+  document.getElementById("current-page-html").innerText = content;
+});
+
+// Code highlighting
+document.addEventListener('DOMContentLoaded', function(event) {
+  hljs.initHighlightingOnLoad();
+});
+
+// Initialize searchbar autosuggest
+document.addEventListener('DOMContentLoaded', function(event) {
+  var comboplete = new Awesomplete('input.dropdown-input', {
+    minChars: 0,
+  });
+  Awesomplete.$('.dropdown-btn').addEventListener("click", function() {
+    if (comboplete.ul.childNodes.length === 0) {
+      comboplete.minChars = 0;
+      comboplete.evaluate();
+    }
+    else if (comboplete.ul.hasAttribute('hidden')) {
+      comboplete.open();
+    }
+    else {
+      comboplete.close();
+    }
+  });
+});
+
+// Function to output domql output
+function domqlsearch(query) {
+  var cursor = new domql.Domql(query);
+  var result = cursor.execute();
+
+  var outp = document.querySelector('#domql-output');
+
+  var content = result ? html_beautify(result.innerHTML, {indent_size: 2}) : null;
+  outp.innerText = content;  
+
+  hljs.highlightBlock(outp);
+}
