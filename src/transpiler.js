335a79418505d52cf8e927c44600230bbf33b19c
Y.J <yuxuan98@hotmail.com>
2019-03-27 14:31:07 -0400

    Add from clause traversal

--- src/transpiler.js
+++ src/transpiler.js
@@ -1,3 +1,13 @@
+function _traverseFrom(node) {
+  var tables = new Set();
+  node.forEach(function (table) {
+    if (table.type !== "table_ref") throw new Error(`Expected ${table} to be a table_ref`);
+
+    tables.add(table.table);
+  });
+  return tables;
+}
+
 /**
  * Traverses the given Abstract Syntax Tree
  * and generates a set of directives
@@ -5,5 +15,15 @@
  * @return {List<string|function>}
  */
 export function transpile(ast) {
+
+  var tables = _traverseFrom(ast.from);
+  _augmentWhere(ast.where, tables);
+
+  var selectors = {};
+  tables.forEach(function (table) {
+    selectors[table] = [""];
+  });
+
+  _traverseWhere(ast.where, selectors);
   return undefined;
 }
