bd446a1b22f606a3ed5613d91213b710dedc91f7
Y.J <yuxuan98@hotmail.com>
2019-03-30 18:38:13 -0400

    Dont transform html into *

--- src/transpiler.js
+++ src/transpiler.js
@@ -119,13 +119,11 @@ function _constructSelectors(node, selectors) {
  * @return {String} compiled DOMString
  */
 function _compileSelectors(selectors, scope) {
-  scope = scope === "html" ? '' : scope + " ";
-
   var outp = [];
   for (var key in selectors) {
     var selector = "";
     for (var i = 0; i < selectors[key].length; i++) {
-      selector += scope + key + selectors[key][i];
+      selector += scope + " " + key + selectors[key][i];
       if (i < selectors[key].length - 1) selector += ", ";
     }
     outp.push(selector);
